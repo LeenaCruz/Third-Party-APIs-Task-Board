4dce9418f0bdd85ef7f2881f86031a65fed7a6e4
Leena <leenacruz@gmail.com>
2024-05-26 19:39:40 -0700

    Add modal form and date picker

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+const taskForm = $('#task-form');
+const addTask = $('#save-task');
+const taskTitle = $("#task-title");
+const taskDate = $("#date-picker");
+const taskDescription = $('#task-description');
+// const taskTitle = document.getElementById('task-title');
+// const taskDate = document.getElementById('date-picker');
+// const taskDescription = document.getElementById('task-description');
+
+
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+
+}
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+
+}
+
+// Todo: create a function to handle adding a new task
+// Me crea un error con el date picker
+function handleAddTask(event) {
+
+    // event.preventDefault();
+    const task = {
+        taskName: taskTitle,
+        taskDate: taskDate,
+        taskDescription: taskDescription,
+    };
+
+ taskList =  JSON.parse(localStorage.getItem("tasks")) || [];
+
+    taskList.push(task);
+
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+
+    console.log(taskList);
+}
+
+// today = dayjs(); taskDate    today -task date  <=3 deadline
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event) {
+
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+
+    // DatePicker
+    $(function () {
+        $("#date-picker").datepicker();
+    });
+
+    // renderTaskList();
+
+    $("#save-task").click(function() {
+        handleAddTask();
+    });
+
+
+    // addTask.on('click', (handleAddTask));
+
+    // addTask.addEventListener('click', handleAddTask);
+
+
+
+
+
+
+});
